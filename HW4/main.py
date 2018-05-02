53f201e16f4605c7db6232700509bd74ff8c8e69
DougBrownWin <DougBrownWin@DESKTOP-9LQ38S7>
2018-05-01 22:25:26 -0600

    made it as far as I could... done for the day..

--- HW4/main.py
+++ HW4/main.py
@@ -8,6 +8,7 @@ main program to run the searching algorithm to solve the coloring graph
 
 import numpy as np
 import fileinput
+from scipy.stats.stats import tiecorrect
 
 
 # get input file from user
@@ -24,7 +25,7 @@ B = 2
 # win lose or tie
 WIN = 1
 TIE = 2
-LOSE = 3
+LOSS = 3
 
 # get input nodes 
 edgeNum = None;
@@ -62,33 +63,115 @@ class Node:
     # parent node
     # player that colors this node
     # number of node
-    # prohibited numbers 
+    # nodes left for A
+    # nodes left for B
+    # total nodes left on graph
     
-    def __init__(self, parent, player, num, prohibited):
+    def __init__(self, parent, player, num, leftA, leftB, leftTot):
         self.parent = parent
         self.player = player
         self.num = num
-        self.prohibited = prohibited
-        print(num)
-        print(prohibited)
-        
-
-def expandNode(node,player):
-    # switch players and expand node
+        self.leftA = leftA
+        self.leftB = leftB
+        self.leftTot = leftTot
     
-    # switch players
+def reduceNode(node, player):
+    # INPUT:
+    #     node: single node
+    #     player: which player chose these nodes
+    # OUTPUT:
+    #    result: win, lose, tie
+    
+    # this procedure reduces the list of nodes
+    # recurisive loop until no more options
+    
+    # alternate player
     if player == A:
         player = B
     else:
         player = A
+
+    #keep list of results from each node
+    # new list of nodes for next layer
+    newNodes = []  
     
-    # available nodes left
-    nodes = []
+    # there are no positions left..., its a tie
+    if not node.leftTot:
+        return TIE
+    
+    # generate new nodes
+    if player == A:
+        # update whats left
+        for num in node.leftA:
+            leftA = node.leftA[:]
+            leftA = checkEdge(num, leftA)
             
-    
-    
+            leftB = node.leftB
+            if num in node.leftB:
+                leftB = node.leftB[:]
+                leftB.remove(num)
+            leftTot = node.leftTot[:]
+            leftTot.remove(num)
+            newNodes.append(Node(node, player, num, leftA, leftB, leftTot))
+    else:
+        # update whats left for B
+        for num in node.leftB:
+            leftB = node.leftB[:]
+            leftB = checkEdge(num, leftB)
+            
+            leftA = node.leftA
+            if num in node.leftA:
+                leftA = node.leftA[:]
+                leftA.remove(num)
+            leftTot = node.leftTot[:]
+            leftTot.remove(num)
+            newNodes.append(Node(node, player, num, leftA, leftB, leftTot))
+            
+    # now step through all new nodes
+    if newNodes:
+        results = []
+        for newNode in newNodes:
+            results.append(reduceNode(newNode, player))
         
+        # choice in logic for the agents min-max
+        if player == A:
+            if WIN in results:
+                return WIN
+            elif TIE in results:
+                return TIE
+            else: 
+                return LOSS
+        if player == B:
+            if LOSS in results:
+                return LOSS
+            elif TIE in results:
+                return TIE 
+            else:
+                return WIN
     
+    # if no new nodes
+    else:
+        if player == A:
+            return LOSS
+        else:
+            return WIN
+            
+        
+def checkEdge(num, left):
+    # INPUT:
+    #    num: is the number of the current node chosen
+    #    left: list of numbers left to chose from
+    # OUTPUT:
+    #     left: updated nodes left
+    
+    for edge in edges:
+        if num in edge:
+            if edge[0] in left:
+                left.remove(edge[0])
+            if edge[1] in left:
+                left.remove(edge[1])
+            
+    return(left)
 
 
 if __name__ == '__main__':
@@ -103,23 +186,26 @@ if __name__ == '__main__':
     # initialize stuff
     nodes = []
     # keep track of nodes
+    
+    
+    
     for num in range(1,nodeNum+1):
-        # prohibited numbers for this node
-        prohibited = []
-        for row in edges:
-            if row[0] == num:
-                prohibited.append(row[1])
-            if row[1] == num:
-                prohibited.append(row[0])
+        # list of nodes left for A and for B
+        leftB = list(range(1,nodeNum + 1))
+        leftA = list(range(1,nodeNum + 1))
+        leftTot = list(range(1,nodeNum+1))
         
-        node = Node(None, player, num, prohibited)
-        nodes.append(node)
-    
-#     # to wonder land
-#     for node in nodes:
-#         result = expandNode(node, player)
-    
-    
+        # num is no longer available
+        leftA.remove(num)
+        leftB.remove(num)
+        leftTot.remove(num)
+        
+        # update leftA based on edges
+        leftA = checkEdge(num, leftA)
+        
+        node = Node(None, player, num, leftA, leftB, leftTot)
+        result = reduceNode(node,player)
+        print(result)
     
     
 
--- HW4/test.py
+++ HW4/test.py
@@ -6,14 +6,18 @@ Created on May 1, 2018
 
 import numpy as np
 
+def loop():
+    for indx in range(10):
+        if(indx == 3):
+            return indx
+
 if __name__ == '__main__':
     pass
 
-
-    array = np.array([[1,2],[1,3],[2,4],[4,1],[5,2]])
-        
-    print(np.max(array))
+    print(loop())
     
-    list = [1,2,3,4]
-    print(1 not in list)
-    print(5 not in list)
+    test = []
+    if test:
+        print('here')
+        
+    
